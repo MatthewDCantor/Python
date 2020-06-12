65f5cd68a38700579334433d267e2f197be0d79e
Matthew Cantor <64717906+MatthewDCantor@users.noreply.github.com>
2020-06-11 19:57:00 -0600

    Update multi_source_BFS_matrix.py

--- Algorithms/multi_source_BFS_matrix.py
+++ Algorithms/multi_source_BFS_matrix.py
@@ -1,12 +1,9 @@
 
 
 """
-1. Pick any node, visit the adjacent unvisited vertex,
-mark it as visited, display it, and insert it in a queue.
-2. If there are no remaining adjacent vertices left,
-remove the first vertex from the queue.
-3. Repeat step 1 and step 2 until the queue
-is empty or the desired node is found.
+This algorithm finds the distance from any point to the nearest source in O(rowxcol) time
+
+
 """
 
 import numpy as np
