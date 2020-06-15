5265ffe449cb0e1c73bd26726f000502422ae9fd
Matthew Cantor <64717906+MatthewDCantor@users.noreply.github.com>
2020-06-15 11:07:13 -0600

    Add files via upload

---
+++ Algorithms/trie.py
@@ -0,0 +1,77 @@
+"""
+Trie is an efficient information reTrieval data structure. Using Trie,
+search complexities can be brought to optimal limit (key length).
+If we store keys in binary search tree, a well balanced BST will need time
+proportional to M * log N, where M is maximum string length
+and N is number of keys in tree. Using Trie, we can search the key in O(M) time.
+However the penalty is on Trie storage requirements.
+(Please refer Applications of Trie for more details)
+
+
+Algorithm to make Trie:
+1. Create root node as null.
+2. Insert key 'example'
+3. I. If root node doesnt have e as a key, then we add a new node with which
+    contains a dictionary with all words which begin with e.
+   II. If e is already a key of the root node then the root node will have a
+   child which contains a TrieNode with e as its key. {'e' : TrieNode}. Make the
+   current node equal to the vale of the key 'e'.
+4. This process continues with 'ex', 'exa', etc.
+"""
+class  TrieNode:
+    """Node in a Trie which contains a dict of all prefixes"""
+
+    def __init__(self):
+
+
+        self.children = {}
+
+class Trie:
+
+
+    def __init__(self):
+
+        self.root = TrieNode()
+
+
+    def insert(self,char):
+
+        #create current node tracker
+        node = self.root
+
+        for i in range(len(char)):
+            # 'e', 'ex', 'exa',..., 'example'
+            key = char[i]
+
+            # if the key is not in the current nodes dict, make a new node
+            # where {key: new Trie Node}
+            if not key in node.children:
+                node.children[key] = TrieNode()
+
+            # make the current node equal to the value of
+            node = node.children[key]
+
+
+    def find(self,char):
+
+        node = self.root
+        string = ""
+        for i in range(len(char)):
+            key = char[i]
+
+            if not key in node.children:
+                return False
+            string = string + char[i]
+            node = node.children[key]
+
+        return string
+
+
+
+
+input =  ["G","G","G","GO","GO","GO","GOO","GOO","GOO","GOOO","GOOO","GOOO"]
+
+t = Trie()
+
+for i in input:
+    t.insert(i)
